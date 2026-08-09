6c98de375d10dc8d6afbeaf78a87f5c5d21ebc05
Khushi <miracle73gkp@gmail.com>
2026-08-09 10:31:07 +0530

    Implement even/odd check in even_odd.cpp
    Fix variable name and output statement in even_odd.cpp.

---
+++ programming/cpp/even_odd.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+  int number;
+
+  cout << "Enter a number: ";
+  cin >> number;
+
+  if (nummber % 2 == 0){
+    cout << "The number is even." << end;
+  }
+  else {
+    cout << "The number is odd." << endl;
+  }
+  return 0;
+}
