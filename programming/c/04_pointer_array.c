a6f0ee401625e86d9b55c65647b7c0e531c0ad3d
Khushi <miracle73gkp@gmail.com>
2026-08-11 21:11:15 +0530

    Add files via upload
    Implement C traversal using pointers.

---
+++ programming/c/04_pointer_array.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+
+    int arr[5] = {10, 20 ,30, 40, 50};
+    
+    int *ptr;
+    ptr = arr;
+
+    printf("The array using pointer: \n");
+    for (int i=0; i<5; i++){
+        printf("Array [%d] = %d\n",i, *(ptr + i));
+    }
+    return 0;
+}
